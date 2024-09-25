767e2f01f16d3d96177aabfc7017402f70914edc
chieby-mwangi <chiebyrah@gmail.com>
2024-09-25 08:57:47 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+console.log(random);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(2, 4, 6, 10, 14, 16, 20);
+console.log(max);
